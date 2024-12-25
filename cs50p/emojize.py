f0d346849328772d0aa3e2b7475b2cd3fefd1658
mucyoelise <mucyo.bryant@gmail.com>
2024-12-25 22:33:00 +0200

    Adding comments

--- cs50p/emojize.py
+++ cs50p/emojize.py
@@ -1,7 +1,11 @@
-import emoji
+import emoji  # Importing the emoji module to add emojis to the output
 
+# Prompt the user to enter their name and capitalize the first letter
 user_input = input('\nEnter your name: ').capitalize()
-heart_emoji = emoji.emojize(':red_heart:', language='alias')
-cry_emoji = emoji.emojize(':sob:', language='alias')
 
-print(f'Hello {user_input}, i{heart_emoji}  you!{cry_emoji}\n')
+# Define the emojis using the emojize function with alias names
+heart_emoji = emoji.emojize(':red_heart:', language='alias')  # Red heart emoji
+cry_emoji = emoji.emojize(':sob:', language='alias')  # Crying emoji
+
+# Print a personalized message including the user's name and the emojis
+print(f'Hello {user_input}, I{heart_emoji} you!{cry_emoji}\n')
