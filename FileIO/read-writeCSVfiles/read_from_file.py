336c7719bad1f9212f684e1a04f7a2868f9d8a79
mucyoelise <mucyo.bryant@gmail.com>
2024-12-28 02:07:29 +0200

    Improving the readability

--- FileIO/read-writeCSVfiles/read_from_file.py
+++ FileIO/read-writeCSVfiles/read_from_file.py
@@ -1,9 +1,13 @@
-import csv
+import csv  # Import the CSV module to handle CSV file operations
+
 #-------------------The start of defining a class in the main function-------------------------
 class Item:
-    pay_rate = 0.80 # The pay rate after 20% of discount applied
-    all_instance = []
-    def __init__(self, name:str, price:float, quantity:int = 0):
+    pay_rate = 0.80  # Class variable that represents the discount rate (80% of the original price)
+    all_instance = []  # Class variable to keep track of all instances of the Item class
+
+    # Constructor (__init__) to initialize the object with name, price, and quantity
+    def __init__(self, name: str, price: float, quantity: int = 0):
+        # Validation checks for the types of name, price, and quantity
         if not isinstance(name, str):
             raise ValueError(f'Oops! The name value = {name}, must be a string type')
         elif not isinstance(price, (int, float)):
@@ -11,56 +15,64 @@ class Item:
         elif not isinstance(quantity, int):
             raise ValueError(f'Oops! The quantity value = {quantity}, must be an integer type')
         
+        # Assign values to instance variables
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        # Add the instance's dictionary (attributes) to the all_instance list
         Item.all_instance.append(self.__dict__)
+
+    # Method to calculate total price in the store (price * quantity)
     def totPriceInTheStore(self):
         return self.price * self.quantity
+
+    # Method to calculate price with discount (using the class variable pay_rate)
     def priceWithDiscount(self):
         return self.price * self.pay_rate
-    
+
     '''
-    Classmethod is used to define other class attributes related to the class and its function/method 
-    has the cls argument as their first argument to contain and access the class attributes. 
+    Class method that reads item data from a CSV file and instantiates Item objects for each row.
+    The classmethod must take 'cls' as its first argument (instead of 'self') to access class attributes.
     '''
     @classmethod
     def instantiate_from_csvfile(cls):
+        # Open the CSV file and read its contents
         with open('read_from_file.csv') as aFile:
-            reader = csv.DictReader(aFile)
-            item_list = list(reader)
+            reader = csv.DictReader(aFile)  # Use DictReader to read the file as a dictionary
+            item_list = list(reader)  # Convert the file reader object to a list of dictionaries
+        # Iterate over the list of items and instantiate objects for each entry
         for item in item_list:
+            # Call the constructor with data from the CSV (Name, Price, Quantity)
             cls(item['Name'], float(item['Price']), int(item['Quantity']))
-    
+
     '''
-    Creating a static method to check if entered number is integer or float type. Note that the static method 
-    attributes or methods can be declared outside of class. Therefore, any methods/functions in static method 
-    don't have object/instance as their first argument for they are just independent themselves.  
+    Static method to check if a given number is an integer (or convertible to integer).
+    Static methods don't take 'self' or 'cls' as the first argument, as they are independent of the class state.
     '''
     @staticmethod
     def isinteger(num):
         try:
-            num = float(num)
+            num = float(num)  # Try converting the input to a float
         except ValueError:
-            print(f'Unable to convert this "{num}" to a number!')
+            print(f'Unable to convert this "{num}" to a number!')  # If it fails, print error and exit
             exit()
 
+        # If the number is a float, check if it's an integer
         if isinstance(num, float):
             return num.is_integer()
         else:
-            return True
+            return True  # If it's already an integer, return True
 
 #----------------------- The end of defining a class in the main function ------------------------------
 
-# Calling instantiate_from_csvfile() function to assign the class attributes to the CSVfile Items' data
-Item.instantiate_from_csvfile() 
+# Calling instantiate_from_csvfile() function to populate the class with data from the CSV file
+Item.instantiate_from_csvfile()
 
-# Checking if all items have been added to the list of instance in the class efficiently
+# Checking if all items have been successfully added to the 'all_instance' list
 print(f'\nThere are {len(Item.all_instance)} instance(s) created from the CSVfile.')
 for item in Item.all_instance: 
-    print(item)
+    print(item)  # Print the dictionary representation of each Item instance
 
-# Checking the number if it's an integer type or a float type depend on the points..
-
+# Prompt the user to enter a number and check if it's an integer or float type
 print(f'{Item.isinteger(input("\nEnter a number to check if it's an integer type: "))}')
