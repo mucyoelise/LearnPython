64947ad31d0f2834ad502a889a36aac63fcbc9fd
mucyoelise <mucyo.bryant@gmail.com>
2024-12-28 02:08:56 +0200

    Improving the readability

--- FileIO/read-writeCSVfiles/writingBook1.py
+++ FileIO/read-writeCSVfiles/writingBook1.py
@@ -1,8 +1,11 @@
 #-----------Writing into the file using WITH operator to open the file----------------
 
-import csv
+import csv  # Import the CSV module to work with CSV files
 
+# Open the 'Book1.csv' file in append mode ('a')
 with open('Book1.csv', 'a') as file:
+    # Create a CSV writer object
     lines = csv.writer(file)
+    
+    # Write a new row to the CSV file with the values ['Jane', '0792347238']
     lines.writerow(['Jane', '0792347238'])
-
