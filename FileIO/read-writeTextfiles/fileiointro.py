2e867b844ba3fd10fc554b72d433b08885dd8e6b
mucyoelise <mucyo.bryant@gmail.com>
2024-12-28 02:10:18 +0200

    Improving readability

--- FileIO/read-writeTextfiles/fileiointro.py
+++ FileIO/read-writeTextfiles/fileiointro.py
@@ -1,8 +1,20 @@
-student_file = open('student_info.txt','r')
+# Opening the file 'student_info.txt' in read mode ('r')
+student_file = open('student_info.txt', 'r')
+
+# Initialize a counter variable to keep track of line numbers
 counter = 1
-for line in student_file: 
+
+# Loop through each line in the file
+for line in student_file:
+    # Print the line number and the line content after stripping leading/trailing whitespaces
     print(f'Line {counter}: {line.strip()}') 
-    # We used strip() to remove some white spaces like (\n) in the file
-    counter+=1
+    # The .strip() method removes any unwanted whitespace characters (e.g., \n at the end of the line)
+    
+    # Increment the counter by 1 to keep track of line numbers
+    counter += 1
+
+# Print an empty line for output formatting
 print()
+
+# Close the file after reading its contents
 student_file.close()
